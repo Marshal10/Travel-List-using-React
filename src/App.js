48971d46ffd7efce549c9d278f16400fdf5d33d2
Marshal10 <marshalmaleane123@gmail.com>
2024-05-27 23:13:27 +0530

    Statistics functionality

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         togglePacked={handleCheckboxToggle}
         deleteItem={handleDeleteItem}
       />
-      <Statistics />
+      <Statistics items={items} />
     </div>
   );
 }
@@ -118,10 +118,26 @@ function Item({ item, togglePacked, deleteItem }) {
   );
 }
 
-function Statistics() {
+function Statistics({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  if (numItems === 0)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
   return (
     <footer className="stats">
-      <em>You have X items on your list,and you have already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `You have ${numItems} items on your list,and you have already packed
+        ${numPacked}(${percentage}%)`}
+      </em>
     </footer>
   );
 }
