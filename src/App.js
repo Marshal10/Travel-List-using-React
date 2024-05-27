ad450210d9c4d494bdf17761733923fdec0cd01f
Marshal10 <marshalmaleane123@gmail.com>
2024-05-27 21:54:09 +0530

    Initial items rendering

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="App">
@@ -38,11 +43,15 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        <li>
-          <input type="checkbox"></input>
-          <span>Item Qty Item Name</span>
-          <button>❌</button>
-        </li>
+        {initialItems.map((item) => (
+          <li key={item.id}>
+            <input type="checkbox"></input>
+            <span>
+              {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+          </li>
+        ))}
       </ul>
       <div className="actions">
         <select>
