693a7cfb192114b5502c195df291b6b82d6a9b2c
Marshal10 <marshalmaleane123@gmail.com>
2024-05-27 22:42:32 +0530

    Adding items via form and rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
+import { useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleNewItem(item) {
+    setItems((i) => [...i, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form addNewItem={handleNewItem} />
+      <PackingList items={items} />
       <Statistics />
     </div>
   );
@@ -22,32 +25,52 @@ function Logo() {
   );
 }
 
-function Form() {
+function Form({ addNewItem }) {
+  const [qty, setQty] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, qty, packed: "false", id: Date.now() };
+    addNewItem(newItem);
+
+    setDescription("");
+    setQty(1);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip ?</h3>
-      <select>
+      <select value={qty} onChange={(e) => setQty((q) => (q = e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription((d) => (d = e.target.value))}
+      ></input>
       <button>Add</button>
     </form>
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <li key={item.id}>
             <input type="checkbox"></input>
             <span>
-              {item.quantity} {item.description}
+              {item.qty} {item.description}
             </span>
             <button>❌</button>
           </li>
