f45be7007e31abc013ac28cd6d439d497b130b05
Marshal10 <marshalmaleane123@gmail.com>
2024-05-27 21:39:39 +0530

    Form Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ export default function App() {
   return (
     <div className="App">
       <Logo />
+      <Form />
     </div>
   );
 }
@@ -11,5 +12,22 @@ function Logo() {
     <div className="logo">
       <p>🏝️ Far Away 🧳</p>
     </div>
+  );
+}
+
+function Form() {
+  return (
+    <form>
+      <h3>What do you need for your 😍 trip ?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item"></input>
+      <button>Add</button>
+    </form>
   );
 }
